a84770110f53ea96e7778b372b6f5c5655087e21
alexycruz1 <xytojr@gmail.com>
2016-02-19 15:20:37 -0600

    Comer y mover y validaciones....

--- AlexyCruz_Examen1.cpp
+++ AlexyCruz_Examen1.cpp
@@ -5,21 +5,40 @@ using std::cout;
 using std::endl;
 
 void crear_llenar(char** matriz);
+void mover1(char** matriz);
+void mover2(char** matriz);
+void imprimir(char** matriz);
 
 int main(int argc, char*argv[]){
 	int size = 7;
 	char** matriz = new char*[size];
 
 	crear_llenar(matriz);
+	imprimir(matriz);
+
+	mover1(matriz);
+	imprimir(matriz);
+	
+
+
+	return 0;
+}
+
+void imprimir(char** matriz){
+	int size = 7;
+
+	for (int i = 0; i < size; i++){
+		cout << i << "  ";	
+	}
+	cout << endl;
 
 	for (int i = 0; i < size; i++){
 		for (int j = 0; j < size; j++){
 			cout << matriz[i][j] << "  ";
 		}
+		cout << i;
 		cout << endl;
 	}
-
-	return 0;
 }
 
 void crear_llenar(char** matriz){
@@ -43,6 +62,97 @@ void crear_llenar(char** matriz){
 				matriz[i][j] = '#';
 			}else{
 				matriz[i][j] = '.';
+			}
+		}
+	}
+}
+
+void mover1(char** matriz){
+	int size = 7;
+	int x1, y1, x2, y2;
+
+	cout << "Ingrese las coordenadas de la pieza a mover (turno de +)" << endl << endl;
+	cout << "Ingrese la coordenada en x: ";
+	cin >> x1;
+	cout << endl;
+
+	cout << "Ingrese la coordenada en y: ";
+	cin >> y1;
+	cout << endl;
+
+	if (x1 > 6 || x1 < 0 || y1 > 6 || y1 < 0 || matriz[x1][y1] != '+'){
+		cout << "No encontramos tu pieza....perdiste el turno" << endl;
+	}else{
+		cout << "Ingresa las coordenadas a la que quieres mover tu pieza" << endl << endl;
+		cout << "Ingrese la coordenada en x: ";
+		cin >> x2;
+		cout << endl;
+
+		cout << "Ingrese la coordenada en y: ";
+		cin >> y2;
+		cout << endl;
+
+		if ((x2 > 6 || x2 < 0 || y2 > 6 || y2 < 0) || ((x2 >= x1 + 3) || (x2 <= x1 - 3) || 
+			(y2 >= y1 +3) || (y2 <= y1 - 3) || matriz[x1][y1] != '+')){
+			cout << "Hiciste un movimiento invalido....perdiste turno" << endl;
+		}else{
+			if ((x2 == x1 + 2) || (x2 == x1 - 2) || (y2 == y1 + 2) || (y2 == y1 - 2)){//if para mover 2 espacios
+				matriz[x1][y1] = '.';
+				matriz[x2][y2] = '+';
+
+			if (matriz[x2 + 1][y2] == '#'){//para comer
+				matriz[x2 + 1][y2] = '+';
+			}
+			if (matriz[x2 - 1][y2] == '#'){
+				matriz[x2 - 1][y2] = '+';
+			}
+			if(matriz[x2][y2 + 1] == '#'){
+				matriz[x2][y2 + 1] = '+';
+			}
+			if(matriz[x2][y2 - 1] == '#'){
+				matriz[x2][y2 - 1] = '+';
+			} 
+			if(matriz[x2 + 1][y2 + 1] == '#'){//inicio de diagonales
+				matriz[x2 + 1][y2 + 1] = '+';
+			}
+			if(matriz[x2 + 1][y2 - 1] == '#'){
+				matriz[x2 + 1][y2 - 1] = '+';
+			}
+			if(matriz[x2 - 1][y2 - 1] == '#'){
+				matriz[x2 - 1][y2 - 1] = '+';
+			}
+			if(matriz[x2 - 1][y2 + 1] == '#'){
+				matriz[x2 - 1][y2 + 1] = '+';
+			}	
+
+			}else if((x2 == x1 + 1) || (x2 == x1 - 1) || (y2 == y1 + 1) || (y2 == y1 - 1)){//if para clonar
+				matriz[x2][y2] = '+';
+
+			if (matriz[x2 + 1][y2] == '#'){//para comer
+				matriz[x2 + 1][y2] = '+';
+			}
+			if (matriz[x2 - 1][y2] == '#'){
+				matriz[x2 - 1][y2] = '+';
+			}
+			if(matriz[x2][y2 + 1] == '#'){
+				matriz[x2][y2 + 1] = '+';
+			}
+			if(matriz[x2][y2 - 1] == '#'){
+				matriz[x2][y2 - 1] = '+';
+			} 
+			if(matriz[x2 + 1][y2 + 1] == '#'){//inicio de diagonales
+				matriz[x2 + 1][y2 + 1] = '+';
+			}
+			if(matriz[x2 + 1][y2 - 1] == '#'){
+				matriz[x2 + 1][y2 - 1] = '+';
+			}
+			if(matriz[x2 - 1][y2 - 1] == '#'){
+				matriz[x2 - 1][y2 - 1] = '+';
+			}
+			if(matriz[x2 - 1][y2 + 1] == '#'){
+				matriz[x2 - 1][y2 + 1] = '+';
+			}
+
 			}
 		}
 	}
